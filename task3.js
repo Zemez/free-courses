20b5fcf3e740ccd0b80445fff3ffac4a8fa80e33
Andrey Inozemtsev <zemez@inbox.ru>
2018-06-04 15:58:22 +0300

    task3.js completed

--- task3.js
+++ task3.js
@@ -1,1 +1,26 @@
 /* Task 3 */
+
+const STR_S = { one: 'секунда', some: 'секунды', many: 'секунд' };
+const STR_M = { one: 'минута', some: 'минуты', many: 'минут' };
+const STR_H = { one: 'час', some: 'часа', many: 'часов' };
+
+function numToString(num, temp) {
+  if (num) {
+    mod = num % 10;
+    return num + ' ' + (num > 10 && num < 20 || mod == 0 || mod >= 5 ?
+                        temp.many : (mod == 1 ? temp.one : temp.some));
+  }
+}
+
+var t1 = Number(process.argv[2]);
+var t2 = Number(process.argv[3]);
+
+var time = t1 + t2;
+var timeS = time % 60;
+var timeM = Math.floor(time / 60) % 60;
+var timeH = Math.floor(time / 3600);
+
+process.stdout.write((timeH ? numToString(timeH, STR_H) + ' ' : '') +
+                     (timeM ? numToString(timeM, STR_M) + ' ' : '') +
+                              numToString(timeS, STR_S) + '\n');
+
