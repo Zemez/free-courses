705d1bb5d0b7d481a614727935f113ff0fb1525c
Andrey Inozemtsev <zemez@inbox.ru>
2018-06-04 14:32:41 +0300

    task2.js completed

--- task2.js
+++ task2.js
@@ -1,1 +1,17 @@
 /* Task 2 */
+
+var a = process.argv[2].split('');
+var b = process.argv[3].split('');
+var s = -1;
+
+if (a.length === b.length) {
+    for (let c = 0; c < a.length; c++) {
+	if (a.every((x, i) => x === b[i])) {
+	    s = c;
+	    break;
+        }
+	a.push(a.shift());
+    }
+}
+
+process.stdout.write((s > a.length - s ? a.length - s : s) + '\n');
