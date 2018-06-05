24393d898cf46328c785ed6ce286e97772f3a99f
Andrey Inozemtsev <zemez@inbox.ru>
2018-06-05 18:28:48 +0300

    task2.rb completed

--- task2.rb
+++ task2.rb
@@ -1,1 +1,18 @@
 # Task 2
+
+a = ARGV[0].split(//)
+b = ARGV[1].split(//)
+s = -1;
+
+if a.length == b.length
+  a.each_index do |c|
+	  if a == b
+	    s = c
+	    break
+	  end #if
+	  a.push(a.shift)
+	end #do
+end #if
+
+puts s > a.length - s ? a.length - s : s
+
