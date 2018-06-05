15290431bb5b8114fcf05d91da8a71bbb7d36b5b
Andrey Inozemtsev <zemez@inbox.ru>
2018-06-05 18:30:06 +0300

    task2.js not important update

--- task2.js
+++ task2.js
@@ -5,13 +5,13 @@ var b = process.argv[3].split('');
 var s = -1;
 
 if (a.length === b.length) {
-    for (let c = 0; c < a.length; c++) {
-	if (a.every((x, i) => x === b[i])) {
+  for (let c = 0; c < a.length; c++) {
+	  if (a.every((x, i) => x === b[i])) {
 	    s = c;
 	    break;
-        }
-	a.push(a.shift());
     }
+	  a.push(a.shift());
+  }
 }
 
 process.stdout.write((s > a.length - s ? a.length - s : s) + '\n');
